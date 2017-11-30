051de77a1ad1e0ad97f727143dc392d23c3e6816
ftt01 <dallatorre.daniele@gmail.com>
2017-11-30 08:46:52 -0700

    Refactored the AbstractNodes.java class

--- src/main/java/org/altervista/growworkinghard/jswmm/dataStructure/hydraulics/nodeObject/AbstractNodes.java
+++
@@ -1,6 +0,0 @@
-package org.altervista.growworkinghard.jswmm.dataStructure.hydraulics.nodeObject;
-
-import org.altervista.growworkinghard.jswmm.dataStructure.INPlocator;
-
-public abstract class AbstractNodes implements INPlocator{
-}
