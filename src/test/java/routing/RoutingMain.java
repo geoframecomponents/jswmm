9966f79a3e2ae65a81774afe8f20381642a0dd2c
ftt01 <dallatorre.daniele@gmail.com>
2018-03-14 16:44:35 +0100

    New setup of Runoff > Routing test main

--- src/test/java/routing/RoutingMain.java
+++ src/test/java/routing/RoutingMain.java
@@ -17,23 +17,66 @@ package routing;
 
 import org.altervista.growworkinghard.jswmm.dataStructure.SWMMobject;
 import org.altervista.growworkinghard.jswmm.routing.Routing;
+import org.altervista.growworkinghard.jswmm.runoff.PreRunoff;
+import org.altervista.growworkinghard.jswmm.runoff.Runoff;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.io.IOException;
 
 public class RoutingMain {
 
     SWMMobject data;
+    PreRunoff preRunoff_1;
+    PreRunoff preRunoff_2;
+    Runoff runoff_1;
+    Runoff runoff_2;
     Routing routing;
+
     @Before
-    public void initialize() throws IOException {
+    public void initialize() {
         data = new SWMMobject();
+
+        preRunoff_1 = new PreRunoff();
+        preRunoff_2 = new PreRunoff();
+
+        preRunoff_1.dataStructure = data;
+        preRunoff_2.dataStructure = data;
+
+        preRunoff_1.areaName = "RG1";
+        preRunoff_2.areaName = "RG2";
+
+        preRunoff_1.initialize();
+        preRunoff_2.initialize();
+
+        preRunoff_1.run();
+        preRunoff_2.run();
     }
 
     @Test
-    public void execute() throws IOException {
+    public void execute() {
+        runoff_1 = new Runoff();
+        runoff_2 = new Runoff();
+
+        runoff_1.dataStructure = data;
+        runoff_1.areaName = "Sub1";
+        runoff_1.nodeName = "N1";
+        runoff_1.adaptedRainfallData = preRunoff_1.getAdaptedRainfallData();
+
+        runoff_2.dataStructure = data;
+        runoff_2.areaName = "Sub2";
+        runoff_2.nodeName = "N1";
+        runoff_2.adaptedRainfallData = preRunoff_2.getAdaptedRainfallData();
+
+        runoff_1.initialize();
+        runoff_2.initialize();
+
+        runoff_1.run();
+        runoff_2.run();
+
         routing = new Routing();
+
+        routing.dataStructure = data;
+        routing.linkName = "L1";
+
         routing.initialize();
         routing.run();
         //routing.test();
