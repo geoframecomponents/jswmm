5304d1a8bb5401d29574fb7d7d1867eff501edaf
ftt01 <dallatorre.daniele@gmail.com>
2019-09-23 10:21:13 +0200

    Refactored CommercialPipeSize structure

--- src/main/java/com/github/geoframecomponents/jswmm/dataStructure/hydraulics/linkObjects/crossSections/pipeSize/CommercialPipeSize.java
+++ src/main/java/com/github/geoframecomponents/jswmm/dataStructure/hydraulics/linkObjects/crossSections/pipeSize/CommercialPipeSize.java
@@ -1,7 +1,33 @@
 package com.github.geoframecomponents.jswmm.dataStructure.hydraulics.linkObjects.crossSections.pipeSize;
 
-public interface CommercialPipeSize {
+import it.blogspot.geoframe.utils.GEOunitsTransform;
 
-    double[] getCommercialDiameter(Double designedDiameter);
+import java.util.Map;
+
+public abstract class CommercialPipeSize {
+
+    Map<Double, Double> pipe;
+
+    // returns commercial size in meters
+    // expects designed size in meters
+    public double[] getCommercialDiameter(Double designedDiameter) {
+        designedDiameter = GEOunitsTransform.meters2millimiters(designedDiameter);
+        double[] commercialSize = null;
+        for(Map.Entry<Double, Double> diameter : pipe.entrySet()) {
+            double innerSize = diameter.getKey();
+            if (innerSize > designedDiameter) {
+                double outerSize = GEOunitsTransform.millimiters2meters(diameter.getValue());
+                innerSize = GEOunitsTransform.millimiters2meters(innerSize);
+                commercialSize = new double[]{innerSize, outerSize};
+                break;
+            }
+        }
+        if (commercialSize == null) {
+            String msg = "Designed diameter " + designedDiameter;
+            msg += " is bigger than the biggest diameter available.";
+            throw new NullPointerException(msg);
+        }
+        return commercialSize;
+    }
 
 }
