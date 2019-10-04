d3da5897536d9159e0283387b955186440b0a802
ftt01 <dallatorre.daniele@gmail.com>
2019-10-04 14:30:29 +0200

    Created new Units abtract class

--- src/main/java/com/github/geoframecomponents/jswmm/dataStructure/Unitable.java
+++ src/main/java/com/github/geoframecomponents/jswmm/dataStructure/Unitable.java
@@ -1,4 +1,19 @@
 package com.github.geoframecomponents.jswmm.dataStructure;
 
-public class Unitable {
+import com.github.geoframecomponents.jswmm.dataStructure.options.units.ProjectUnits;
+
+public abstract class Unitable {
+    ProjectUnits units;
+
+    public Unitable(ProjectUnits units) {
+        this.units = units;
+    }
+
+    public ProjectUnits getUnits() {
+        return units;
+    }
+
+    public void setUnits(ProjectUnits units) {
+        this.units = units;
+    }
 }
