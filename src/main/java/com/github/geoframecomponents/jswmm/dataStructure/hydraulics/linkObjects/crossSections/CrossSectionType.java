0c1ad20008a044da1191550abee230d4d42fa3a0
ftt01 <dallatorre.daniele@gmail.com>
2019-10-28 09:56:09 +0100

    Added GPL and some comments to test JavaDoc

--- src/main/java/com/github/geoframecomponents/jswmm/dataStructure/hydraulics/linkObjects/crossSections/CrossSectionType.java
+++ src/main/java/com/github/geoframecomponents/jswmm/dataStructure/hydraulics/linkObjects/crossSections/CrossSectionType.java
@@ -1,24 +1,42 @@
 /*
- * This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+JSWMM: Reimplementation of EPA SWMM in Java
+Copyright (C) 2019 Daniele Dalla Torre (ftt01)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 package com.github.geoframecomponents.jswmm.dataStructure.hydraulics.linkObjects.crossSections;
 
+/**
+ * Class that defines the properties of the pipe.
+ *
+ * Extending this abstract class is possible to define personalized cross section type of pipe.
+ * @author ftt01 (dallatorre.daniele@gmail.com)
+ */
 public interface CrossSectionType {
 
+    /**
+     * Defines if the cross section defined always increase its capacity increasing the discharge.
+     * @return <b>true</b> id the capacity always increase.
+     */
     public Boolean getAlwaysIncrease();
 
+    /**
+     * <img src="./doc-files/signature.png" />
+     * @param theta
+     * @return
+     */
     public Double derivatedSectionFactor(Double theta);
 
     Double getDepthFull();
